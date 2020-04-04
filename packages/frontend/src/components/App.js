76b8031b928d7c6b44859d7f7e3584f040313f02
muhammad-arslan-khan <muhammad-arslan.khan@auto1.com>
2020-04-04 19:05:53 +0200

    basic styling

--- packages/frontend/src/components/App.js
+++ packages/frontend/src/components/App.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import Card from './shared/Card'
+import styles from './App.module.css'
 
 function App() {
-  return <input type="text" />
+  return (
+    <>
+      <Card>
+        <label htmlFor="amount">Amount</label>
+        <input
+          type="number"
+          className={styles.input}
+          id="amount"
+          min="10000"
+          max="100000"
+        />
+
+        <label htmlFor="duration" className={styles.mt20}>
+          Duration
+        </label>
+        <input
+          type="number"
+          className={styles.input}
+          id="duration"
+          min="1"
+          max="5"
+        />
+
+        <button>Calculate</button>
+      </Card>
+
+      <Card>
+        <p>
+          Monthly Installment: <span>5390.61 EUR</span>
+        </p>
+      </Card>
+    </>
+  )
 }
 
 export default App
--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -1,14 +1,12 @@
 import React, { StrictMode } from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import CssBaseline from '@material-ui/core/CssBaseline'
 import './index.css'
 import App from './components/App'
 import store from './store/store'
 
 render(
   <StrictMode>
-    <CssBaseline />
     <Provider store={store}>
       <App />
     </Provider>
